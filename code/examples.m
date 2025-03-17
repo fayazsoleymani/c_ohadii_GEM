d7b48a5be7f37c2ac586cc501a28288311641a83
fayazsoleymani <arti.sb@yahoo.com>
2025-03-17 11:10:47 +0100

    initial commit

---
+++ code/examples.m
@@ -0,0 +1,17 @@
+% model reconstruction from kegg and metacyc
+proteinFastaFile= fullfile('..', 'data', 'ncbi', 'protein.faa');
+draft= draftReconstructionKeggMetacyc(proteinFastaFile);
+
+% exchange reactions
+exchange2lbDict= dictionary({'WATER'}, -10, {'PROTON'}, -10, {'Pi'}, -10, ...
+    {'AMMONIUM'}, -1, {'SULFATE'}, -10, {'FE+2'}, -10, {'CARBON-DIOXIDE'}, ...
+    -2, {'MG+2'}, -10, {'C00205'}, -80, {'OXYGEN-MOLECULE'}, -10);
+model= modifyExchangeRxns(model, exchange2lbDict);
+
+model= addBiomassRxn(model, fullfile('..', 'data', 'biomass_all.tsv'));
+
+
+%%
+clc;clear;
+model= readCbModel('../models/eciCO1515_hetero.xml');
+
